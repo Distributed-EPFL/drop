0bb08f8757d254407f72019b1301667cad58e4b8
Matteo Monti <matteo.monti@epfl.ch>
2019-05-20 18:01:01 +0200

    Implement `Root` for booleans and integers

--- src/bytewise/root.rs
+++ src/bytewise/root.rs
@@ -1,5 +1,6 @@
 // Dependencies
 
+use std::convert::TryInto;
 use super::sink::Sink;
 use super::source::Source;
 
@@ -9,3 +10,33 @@ pub trait Root : Sized {
     fn dump<To: Sink>(&self, to: &mut To) -> Result<(), To::Error>;
     fn load<From: Source>(from: &mut From) -> Result<Self, From::Error>;
 }
+
+// Implementations
+
+impl Root for bool {
+    fn dump<To: Sink>(&self, to: &mut To) -> Result<(), To::Error> {
+        to.push(if *self { &[1] } else { &[0] })
+    }
+
+    fn load<From: Source>(from: &mut From) -> Result<Self, From::Error> {
+        let value = from.pop(1)?[0] != 0;
+        Ok(value)
+    }
+}
+
+macro_rules! implement {
+    ($($type:ty: $size:expr), *) => ($(
+        impl Root for $type {
+            fn dump<To: Sink>(&self, to: &mut To) -> Result<(), To::Error> {
+                to.push(&self.to_le_bytes())
+            }
+
+            fn load<From: Source>(from: &mut From) -> Result<Self, From::Error> {
+                let value = Self::from_le_bytes(from.pop($size)?.try_into().unwrap());
+                Ok(value)
+            }
+        }
+    )*);
+}
+
+implement!(i8: 1, i16: 2, i32: 4, i64: 8, i128: 16, u8: 1, u16: 2, u32: 4, u64: 8, u128: 16);
