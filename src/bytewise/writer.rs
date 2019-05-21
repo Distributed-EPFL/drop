714cd77c8e554750484d61bcec111bba4d0ea495
Matteo Monti <matteo.monti@epfl.ch>
2019-05-21 17:45:26 +0200

    Test `Writer` implementations for `Source`

--- src/bytewise/writer.rs
+++ src/bytewise/writer.rs
@@ -26,3 +26,53 @@ impl<Visitor: Source> Writer for Visitor {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::infallible::Infallible;
+
+    // Structs
+
+    struct Sponge {
+        bytes: &'static [u8],
+        cursor: usize
+    }
+
+    // Implementations
+
+    impl Sponge {
+        fn new(bytes: &'static [u8]) -> Sponge {
+            Sponge{bytes, cursor: 0}
+        }
+    }
+
+    impl Source for Sponge {
+        type Error = Infallible;
+
+        fn pop(&mut self, size: usize) -> Result<&[u8], Self::Error> {
+            let slice = &self.bytes[self.cursor..(self.cursor + size)];
+            self.cursor += size;
+            Ok(slice)
+        }
+    }
+
+    // Test cases
+
+    #[test]
+    fn source() {
+        let mut sponge = Sponge::new(&[0x44, 0x00, 0x00, 0x00, 0x55, 0x66, 0x00][..]);
+
+        let mut value: u32 = 0;
+        sponge.visit(&mut value).unwrap();
+        assert_eq!(value, 0x44);
+
+        let mut value: u8 = 0;
+        sponge.write(&mut value).unwrap();
+        assert_eq!(value, 0x55);
+
+        let mut value: u16 = 0;
+        value.accept(&mut sponge).unwrap();
+        assert_eq!(value, 0x66);
+    }
+}
