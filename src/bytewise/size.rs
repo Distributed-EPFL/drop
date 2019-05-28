a1542e7f7637ddf5db8ab1ba87c8615e0a9be41d
Matteo Monti <matteo.monti@epfl.ch>
2019-05-28 16:29:47 +0200

    Implement `Mul` for `Size`

--- src/bytewise/size.rs
+++ src/bytewise/size.rs
@@ -1,6 +1,7 @@
 // Dependencies
 
 use std::ops::Add;
+use std::ops::Mul;
 
 // Enums
 
@@ -24,6 +25,18 @@ impl Add<Size> for Size {
     }
 }
 
+impl Mul<usize> for Size {
+    type Output = Size;
+
+    fn mul(self, rhs: usize) -> Size {
+        if let Size::Fixed(left) = self {
+            Size::Fixed(left * rhs)
+        } else {
+            Size::Variable
+        }
+    }
+}
+
 // Tests
 // #[kcov(exclude)]
 
@@ -37,5 +50,8 @@ mod tests {
         assert_eq!(Size::Fixed(4) + Size::Variable, Size::Variable);
         assert_eq!(Size::Variable + Size::Fixed(5), Size::Variable);
         assert_eq!(Size::Variable + Size::Variable, Size::Variable);
+
+        assert_eq!(Size::Fixed(4) * 4, Size::Fixed(16));
+        assert_eq!(Size::Variable * 4, Size::Variable);
     }
 }
