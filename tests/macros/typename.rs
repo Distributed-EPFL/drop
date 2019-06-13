9e51601ce7ea800104c32fe859ba782ed8d8ec2e
Matteo Monti <matteo.monti@epfl.ch>
2019-06-13 14:44:33 +0200

    Test `Derive(typename)` macro

--- tests/macros/typename.rs
+++ tests/macros/typename.rs
@@ -11,5 +11,18 @@ struct MyOtherStruct<T>(T);
 
 #[test]
 fn develop() {
-    println!("{}", <[MyOtherStruct<MyOtherStruct<(u32, u64, f64, std::collections::BinaryHeap<&'static MyStruct>, std::vec::Vec<String>)>>; 2]>::typename());
+    let typename = format!("{}", std::collections::BinaryHeap::<&'static MyStruct>::typename());
+    assert_eq!(typename, "BinaryHeap<&'static MyStruct>");
+
+    let typename = format!("{}", <(u32, u64, f64, std::collections::BinaryHeap<&'static MyStruct>, std::vec::Vec<String>)>::typename());
+    assert_eq!(typename, "(u32, u64, f64, BinaryHeap<&'static MyStruct>, Vec<String>)");
+
+    let typename = format!("{}", MyOtherStruct::<(u32, u64, f64, std::collections::BinaryHeap<&'static MyStruct>, std::vec::Vec<String>)>::typename());
+    assert_eq!(typename, "MyOtherStruct<(u32, u64, f64, BinaryHeap<&'static MyStruct>, Vec<String>)>");
+
+    let typename = format!("{}", MyOtherStruct::<MyOtherStruct<(u32, u64, f64, std::collections::BinaryHeap<&'static MyStruct>, std::vec::Vec<String>)>>::typename());
+    assert_eq!(typename, "MyOtherStruct<MyOtherStruct<(u32, u64, f64, BinaryHeap<&'static MyStruct>, Vec<String>)>>");
+
+    let typename = format!("{}", <[MyOtherStruct<MyOtherStruct<(u32, u64, f64, std::collections::BinaryHeap<&'static MyStruct>, std::vec::Vec<String>)>>; 2]>::typename());
+    assert_eq!(typename, "[MyOtherStruct<MyOtherStruct<(u32, u64, f64, BinaryHeap<&'static MyStruct>, Vec<String>)>>; 2]");
 }
