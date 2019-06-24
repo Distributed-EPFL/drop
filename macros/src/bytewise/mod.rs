575da8cce4b67edb29516cbb9ac9a56ff31e1260
Matteo Monti <matteo.monti@epfl.ch>
2019-06-24 11:33:51 +0200

    Refactor `impls` module into dedicated `readable` module

--- macros/src/bytewise/mod.rs
+++ macros/src/bytewise/mod.rs
@@ -1,7 +1,7 @@
 // Modules
 
-mod impls;
 mod parse;
+mod readable;
 
 // Dependencies
 
@@ -13,7 +13,7 @@ use syn::parse_macro_input;
 pub fn readable(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let configuration = parse::configuration(&input);
-    let response = impls::readable(&configuration).into();
+    let response = readable::readable(&configuration).into();
     println!("{}", response);
     response
 }
