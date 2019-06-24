dacbb6acc9be347544d562a3079ee2f2840e97ab
Matteo Monti <matteo.monti@epfl.ch>
2019-06-24 17:11:18 +0200

    Test `#[derive(Readable)]` on `enum`s

--- tests/macros/bytewise.rs
+++ tests/macros/bytewise.rs
@@ -21,11 +21,10 @@ struct Unnamed(#[bytewise] u32, #[bytewise] String);
 struct NamedEmpty {}
 
 #[derive(Readable)]
-#[allow(dead_code)]
 struct NamedPartial {
-    x: u32,
+    _x: u32,
     #[bytewise] y: u32,
-    z: u32,
+    _z: u32,
     #[bytewise] w: String
 }
 
@@ -33,6 +32,25 @@ struct NamedPartial {
 struct Named {
     #[bytewise] x: u32,
     #[bytewise] y: String
+}
+
+#[derive(Readable)]
+enum Enum {
+    Unit,
+    UnnamedEmpty(),
+    UnnamedPartial(u32, #[bytewise] u32, u32, #[bytewise] String),
+    Unnamed(#[bytewise] u32, #[bytewise] String),
+    NamedEmpty {},
+    NamedPartial {
+        _x: u32,
+        #[bytewise] y: u32,
+        _z: u32,
+        #[bytewise] w: String
+    },
+    Named {
+        #[bytewise] x: u32,
+        #[bytewise] y: String
+    }
 }
 
 // Test cases
@@ -44,6 +62,14 @@ fn readable() {
     assert_eq!(bytewise::serialize(&UnnamedPartial(4, 5, 6, "seven".to_string())).unwrap(), [5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
     assert_eq!(bytewise::serialize(&Unnamed(5, "seven".to_string())).unwrap(), [5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
     assert_eq!(bytewise::serialize(&NamedEmpty{}).unwrap(), []);
-    assert_eq!(bytewise::serialize(&NamedPartial{x: 4, y: 5, z: 6, w: "seven".to_string()}).unwrap(), [5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    assert_eq!(bytewise::serialize(&NamedPartial{_x: 4, y: 5, _z: 6, w: "seven".to_string()}).unwrap(), [5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
     assert_eq!(bytewise::serialize(&Named{x: 5, y: "seven".to_string()}).unwrap(), [5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+
+    assert_eq!(bytewise::serialize(&Enum::Unit).unwrap(), [0]);
+    assert_eq!(bytewise::serialize(&Enum::UnnamedEmpty()).unwrap(), [1]);
+    assert_eq!(bytewise::serialize(&Enum::UnnamedPartial(4, 5, 6, "seven".to_string())).unwrap(), [2, 5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    assert_eq!(bytewise::serialize(&Enum::Unnamed(5, "seven".to_string())).unwrap(), [3, 5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    assert_eq!(bytewise::serialize(&Enum::NamedEmpty{}).unwrap(), [4]);
+    assert_eq!(bytewise::serialize(&Enum::NamedPartial{_x: 4, y: 5, _z: 6, w: "seven".to_string()}).unwrap(), [5, 5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    assert_eq!(bytewise::serialize(&Enum::Named{x: 5, y: "seven".to_string()}).unwrap(), [6, 5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
 }
