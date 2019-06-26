9bab0f1a3c3d96be6c9dc17d0190732eda9436c6
Matteo Monti <matteo.monti@epfl.ch>
2019-06-26 16:02:19 +0200

    Add `destruct` methods to `Struct` and `Variant`

--- macros/src/bytewise/configuration.rs
+++ macros/src/bytewise/configuration.rs
@@ -39,3 +39,28 @@ pub struct Field {
     pub ty: TokenStream,
     pub marked: bool
 }
+
+// Implementations
+
+impl Struct {
+    pub fn destruct(&self) -> TokenStream {
+        destruct(&self.ident, &self.naming, &self.fields)
+    }
+}
+
+impl Variant {
+    pub fn destruct(&self) -> TokenStream {
+        destruct(&self.ident, &self.naming, &self.fields)
+    }
+}
+
+// Functions
+
+fn destruct(ident: &TokenStream, naming: &Naming, fields: &Vec<Field>) -> TokenStream {
+    let fields = fields.into_iter().map(|field| &field.destruct);
+    match naming {
+        Naming::Named => quote!(#ident{#(#fields),*}),
+        Naming::Unnamed => quote!(#ident(#(#fields),*)),
+        Naming::Unit => quote!(#ident)
+    }
+}
