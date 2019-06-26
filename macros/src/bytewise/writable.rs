c14f3edefb2538196228f886b19542a6f3dad930
Matteo Monti <matteo.monti@epfl.ch>
2019-06-26 15:05:59 +0200

    Implement `#[derive(Writable)]` for `enum` types

--- macros/src/bytewise/writable.rs
+++ macros/src/bytewise/writable.rs
@@ -2,6 +2,7 @@
 
 use proc_macro2::TokenStream;
 use quote::quote;
+use super::load;
 use super::parse::Configuration;
 use super::parse::Naming;
 
@@ -25,8 +26,18 @@ pub fn writable(configuration: &Configuration) -> TokenStream {
             }
         },
         Configuration::Enum{ident: item_ident, variants} => {
-            let arms = variants.into_iter().enumerate().map(|(discriminant, variant)| {
+            let discriminant_arms = variants.into_iter().enumerate().map(|(discriminant, variant)| {
+                let variant_ident = &variant.ident;
                 let discriminant = discriminant as u8;
+
+                match variant.naming {
+                    Naming::Named => quote!(#item_ident::#variant_ident{..} => #discriminant),
+                    Naming::Unnamed => quote!(#item_ident::#variant_ident(..) => #discriminant),
+                    Naming::Unit => quote!(#item_ident::#variant_ident => #discriminant)
+                }
+            });
+
+            let write_arms = variants.into_iter().map(|variant| {
                 let variant_ident = &variant.ident;
 
                 let destructs = (&variant.fields).into_iter().map(|field| &field.destruct);
@@ -40,8 +51,18 @@ pub fn writable(configuration: &Configuration) -> TokenStream {
                 let visits = acceptors.map(|acceptor| &acceptor.destruct).map(|ident| quote!(visitor.visit(#ident)?;));
 
                 quote! {
-                    (#discriminant, #destruct) => {
+                    #destruct => {
                         #(#visits)*
+                    }
+                }
+            });
+
+            let load_arms = variants.into_iter().enumerate().map(|(discriminant, variant)| {
+                let discriminant = discriminant as u8;
+                let body = load::variant(item_ident, variant);
+                quote! {
+                    #discriminant => {
+                        #body
                     }
                 }
             });
@@ -51,9 +72,17 @@ pub fn writable(configuration: &Configuration) -> TokenStream {
                     const SIZE: drop::bytewise::Size = drop::bytewise::Size::variable();
                     fn accept<Visitor: drop::bytewise::Writer>(&mut self, visitor: &mut Visitor) -> Result<(), drop::bytewise::WriteError> {
                         let discriminant = u8::load(visitor)?;
-                        match(discriminant, self) {
-                            #(#arms,)*
-                            _ => unimplemented!()
+                        if discriminant == match self {
+                            #(#discriminant_arms),*
+                        } {
+                            match self {
+                                #(#write_arms),*
+                            }
+                        } else {
+                            *self = match discriminant {
+                                #(#load_arms),*
+                                _ => return Err(drop::bytewise::WritableError::new("UnexpectedDeterminant").into())
+                            };
                         }
 
                         Ok(())
