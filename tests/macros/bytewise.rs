64d9bf98692f9f45e0ab17aad624969b152f56f5
Matteo Monti <matteo.monti@epfl.ch>
2019-06-27 14:47:48 +0200

    Test `#[derive(Writable)]` for `struct`s

--- tests/macros/bytewise.rs
+++ tests/macros/bytewise.rs
@@ -1,28 +1,31 @@
 // Dependencies
 
 use drop::bytewise;
+use drop::bytewise::Deserializer;
 use drop::bytewise::Load;
 use drop::bytewise::Readable;
 use drop::bytewise::Writable;
+use drop::bytewise::Writer;
+use std::fmt::Debug;
 
 // Structs
 
-#[derive(Readable, Writable, Load, Debug, PartialEq)]
+#[derive(Readable, Writable, Load, Default, Debug, PartialEq)]
 struct Unit;
 
-#[derive(Readable, Writable, Load, Debug, PartialEq)]
+#[derive(Readable, Writable, Load, Default, Debug, PartialEq)]
 struct UnnamedEmpty();
 
-#[derive(Readable, Writable, Load, Debug, PartialEq)]
+#[derive(Readable, Writable, Load, Default, Debug, PartialEq)]
 struct UnnamedPartial(u32, #[bytewise] u32, u32, #[bytewise] String);
 
-#[derive(Readable, Writable, Load, Debug, PartialEq)]
+#[derive(Readable, Writable, Load, Default, Debug, PartialEq)]
 struct Unnamed(#[bytewise] u32, #[bytewise] String);
 
-#[derive(Readable, Writable, Load, Debug, PartialEq)]
+#[derive(Readable, Writable, Load, Default, Debug, PartialEq)]
 struct NamedEmpty {}
 
-#[derive(Readable, Writable, Load, Debug, PartialEq)]
+#[derive(Readable, Writable, Load, Default, Debug, PartialEq)]
 struct NamedPartial {
     _x: u32,
     #[bytewise] y: u32,
@@ -30,7 +33,7 @@ struct NamedPartial {
     #[bytewise] w: String
 }
 
-#[derive(Readable, Writable, Load, Debug, PartialEq)]
+#[derive(Readable, Writable, Load, Default, Debug, PartialEq)]
 struct Named {
     #[bytewise] x: u32,
     #[bytewise] y: String
@@ -55,6 +58,18 @@ enum Enum {
     }
 }
 
+// Functions
+
+fn write_default<Type: Default + Writable + PartialEq + Debug>(reference: Type, bytes: &[u8]) {
+    write_on(reference, Type::default(), bytes);
+}
+
+fn write_on<Type: Default + Writable + PartialEq + Debug>(reference: Type, mut item: Type, bytes: &[u8]) {
+    let mut deserializer = Deserializer(bytes);
+    deserializer.visit(&mut item).unwrap();
+    assert_eq!(item, reference);
+}
+
 // Test cases
 
 #[test]
@@ -74,6 +89,20 @@ fn readable() {
     assert_eq!(bytewise::serialize(&Enum::NamedEmpty{}).unwrap(), [4]);
     assert_eq!(bytewise::serialize(&Enum::NamedPartial{_x: 4, y: 5, _z: 6, w: "seven".to_string()}).unwrap(), [5, 5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
     assert_eq!(bytewise::serialize(&Enum::Named{x: 5, y: "seven".to_string()}).unwrap(), [6, 5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+}
+
+#[test]
+fn writable() {
+    write_default(Unit, &[]);
+    write_default(UnnamedEmpty(), &[]);
+    write_default(UnnamedPartial(0, 5, 0, "seven".to_string()), &[5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    write_default(Unnamed(5, "seven".to_string()), &[5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    write_default(NamedEmpty{}, &[]);
+    write_default(NamedPartial{_x: 0, y: 5, _z: 0, w: "seven".to_string()}, &[5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    write_default(Named{x: 5, y: "seven".to_string()}, &[5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+
+    write_on(UnnamedPartial(1, 5, 1, "seven".to_string()), UnnamedPartial(1, 4, 1, "six".to_string()), &[5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
+    write_on(NamedPartial{_x: 1, y: 5, _z: 1, w: "seven".to_string()}, NamedPartial{_x: 1, y: 4, _z: 1, w: "six".to_string()}, &[5, 0, 0, 0, 5, 115, 101, 118, 101, 110]);
 }
 
 #[test]
