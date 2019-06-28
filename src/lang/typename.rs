da0d3d7d1d5e912ee49975631e777428c29b4199
Matteo Monti <matteo.monti@epfl.ch>
2019-06-28 15:35:17 +0200

    Implement `Typename` for `isize` and `usize`

--- src/lang/typename.rs
+++ src/lang/typename.rs
@@ -27,7 +27,7 @@ macro_rules! implement {
     )*);
 }
 
-implement!(i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, f32, f64, str, String);
+implement!(i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, isize, usize, f32, f64, str, String);
 
 // Arrays
 
