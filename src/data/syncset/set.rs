a9f2c44fcfd66018e5ccd75f6b30df11bf41dec6
Haley Owsianko <haley.owsianko@gmail.com>
2019-09-26 13:29:00 +0200

    Add `Set` datatype, for Synchronization purposes

---
+++ src/data/syncset/set.rs
@@ -0,0 +1,39 @@
+use super::path::PrefixedPath;
+use crate::crypto::hash::Digest;
+use crate::bytewise::Readable;
+use super::Node;
+
+// Data structure used to synchronize two SyncSets
+pub enum Set<Data> {
+    // Lightweight alternative, only contains the hash of 
+    // the sub-tree at prefix
+    LabelSet {
+        prefix: PrefixedPath,
+        label: Digest
+    },
+
+    // Heavy alternative, contains all the data of a sub-tree at
+    // a given prefix
+    DataSet {
+        underlying: Vec<Data>,
+        prefix: PrefixedPath,
+        dump: bool
+    }
+}
+
+impl<Data: Readable + PartialEq + Clone> Set<Data> {
+
+    // Constructors, for ease of use
+    pub(super) fn new_dataset(prefix: PrefixedPath, node: &Node<Data>, dump: bool) -> Set<Data> {
+        let mut underlying: Vec<Data> = Vec::with_capacity(node.size());
+        node.traverse(&mut |elem| underlying.push(elem.clone()));
+
+        Set::DataSet{underlying, prefix, dump}
+    }
+
+    pub fn new_empty_dataset(prefix: PrefixedPath, dump: bool) -> Set<Data> {
+        let underlying = Vec::new();
+        Set::DataSet{underlying, prefix, dump}
+    }
+
+}
