d0d03b9d1d1bc44e54a2cab34d857c75c3329e57
Haley Owsianko <haley.owsianko@gmail.com>
2019-09-28 12:03:33 +0200

    Implement left/right extensions of a path

--- src/data/syncset/path.rs
+++ src/data/syncset/path.rs
@@ -7,6 +7,7 @@ const BITS_IN_BYTE: usize = 8;
 // Structs 
 #[derive(Clone)]
 pub struct HashPath (pub(super) Digest);
+#[derive(Clone)]
 pub struct PrefixedPath {
     inner: Vec<u8>,
     depth: usize
@@ -28,6 +29,10 @@ impl Direction {
             Direction::Left
         }
     }
+
+    pub fn to_bit(&self) -> bool {
+        self == &Direction::Right
+    }
 }
 
 impl HashPath {
@@ -46,6 +51,38 @@ impl HashPath {
 }
 
 impl PrefixedPath {
+    pub fn add_one(&self, dir: Direction) -> PrefixedPath {
+        debug_assert!(self.depth <= HashPath::NUM_BITS);
+        let mut new_inner = self.inner.clone();
+        let new_depth = self.depth+1;
+        if self.depth % BITS_IN_BYTE == 0 {
+            new_inner.push(0)
+        };
+        let (byte_idx, bit_idx) = split_bits(new_depth-1);
+        let current_byte = new_inner.get_mut(byte_idx).unwrap();
+
+        let new_bit = dir.to_bit();
+
+        if new_bit == true {
+            // Set the new bit
+            let mask = get_mask(bit_idx);
+            *current_byte = *current_byte | mask;
+        } else {
+            // Unset the new bit
+            let mask = !get_mask(bit_idx);
+            *current_byte = *current_byte & mask;
+        }
+        PrefixedPath{inner: new_inner, depth: new_depth}
+    }
+
+    pub fn left(&self) -> PrefixedPath {
+        self.add_one(Direction::Left)
+    }
+
+    pub fn right(&self) -> PrefixedPath {
+        self.add_one(Direction::Right)
+    }
+
     pub fn at(&self, idx: usize) -> Option<Direction> {
         if idx < self.depth {
             let (byte_idx, bit_idx) = split_bits(idx);
@@ -56,7 +93,7 @@ impl PrefixedPath {
     }
 
     pub fn new(depth: usize, inner: Vec<u8>) -> Result<PrefixedPath, PathLengthError> {
-        if inner.len() < (depth+BITS_IN_BYTE-1)/BITS_IN_BYTE {
+        if inner.len() != (depth+BITS_IN_BYTE-1)/BITS_IN_BYTE {
             Err(PathLengthError::new())
         } else {
             Ok(PrefixedPath{depth, inner})
@@ -65,34 +102,30 @@ impl PrefixedPath {
 
     // TODO clean up?
     pub fn is_prefix_of(&self, rhs: &HashPath) -> bool {
-       if self.depth > HashPath::NUM_BITS {
-           false
-       } else {
-           let (num_full_bytes, overflow_bits) = split_bits(self.depth);
-           for i in 0..num_full_bytes {
-               if let Some(byte) = self.inner.get(i) {
-                   if *byte != (rhs.0).0[i] {
-                       return false;
-                   }
-               } else {
-                   return false
-               };
-           }
-           if overflow_bits > 0 {
-               if let Some(last_byte_left) = self.inner.get(num_full_bytes) {
-                   let last_byte_right = (rhs.0).0[num_full_bytes];
-                   let shift_amount = overflow_bits;
-                   let left_masked = last_byte_left >> shift_amount;
-                   let right_masked = last_byte_right >> shift_amount;
-                   if left_masked != right_masked {
-                       return false;
-                   }
-               } else {
-                   return false
-               }
-           }
-           true
-       }
+        let (num_full_bytes, overflow_bits) = split_bits(self.depth);
+        for i in 0..num_full_bytes {
+            if let Some(byte) = self.inner.get(i) {
+                if *byte != (rhs.0).0[i] {
+                    return false;
+                }
+            } else {
+                return false
+            };
+        }
+        if overflow_bits > 0 {
+            if let Some(last_byte_left) = self.inner.get(num_full_bytes) {
+                let last_byte_right = (rhs.0).0[num_full_bytes];
+                let shift_amount = overflow_bits;
+                let left_masked = last_byte_left >> shift_amount;
+                let right_masked = last_byte_right >> shift_amount;
+                if left_masked != right_masked {
+                    return false;
+                }
+            } else {
+                return false
+            }
+        }
+        true
     }
 }
 
@@ -105,9 +138,12 @@ fn split_bits(to_split: usize) -> (usize, usize) {
 
 // Checks if the i-th bit is set in a byte
 fn is_bit_set(byte: u8, bit_idx: usize) -> bool {
-    let mask = 1 << (BITS_IN_BYTE - bit_idx - 1);
-    let masked = byte & mask;
+    let masked = byte & get_mask(bit_idx);
     masked != 0
+}
+
+fn get_mask(bit_idx: usize) -> u8 {
+    1 << (BITS_IN_BYTE - bit_idx - 1)
 }
 
 
@@ -115,6 +151,27 @@ fn is_bit_set(byte: u8, bit_idx: usize) -> bool {
 mod tests {
     use super::*;
     use std::convert::TryFrom;
+
+    #[test]
+    fn extension() {
+        let mut path = PrefixedPath::new(0, Vec::new()).unwrap();
+        for i in 0..HashPath::NUM_BITS {
+            assert_eq!(path.inner.len(), (i+BITS_IN_BYTE-1)/BITS_IN_BYTE);
+            if i % 2 == 1 {
+                path = path.left()
+            } else {
+                path = path.right()
+            }
+        }
+
+        for i in 0..HASH_SIZE {
+            if let Some(b) = path.inner.get(i) {
+                assert_eq!(b, &0xAA)
+            } else {
+                panic!("Inner vector was too short")
+            }
+        }
+    }
 
     #[test]
     fn bits() {
@@ -132,17 +189,18 @@ mod tests {
     #[test]
     fn prefixes() {
         let full = HashPath(Digest::try_from("0101010101000000000000000000000000000000000000000000000000000000").unwrap());
-        assert_eq!((full.0).0[0], 0b00000001);
 
-        let pref = PrefixedPath::new(7, vec!(0)).expect("Prefixed path coudn't be created");
-        assert_eq!(*pref.inner.get(0).unwrap(), 0b00000000);
+        let pref1 = PrefixedPath::new(7, vec!(0)).expect("Prefixed path coudn't be created");
+        assert!(pref1.is_prefix_of(&full), "prefix1 returned false");
 
-        assert!(pref.is_prefix_of(&full), "prefix didn't return true");
+        let pref2 = PrefixedPath::new(8, vec!(0b0000_0001)).unwrap();
+        assert!(pref2.is_prefix_of(&full), "prefix2 returned false");
 
-        let pref2 = PrefixedPath::new(8, vec!(1)).unwrap();
-        assert_eq!(pref2.inner[0], 0b00000001);
+        let pref3 = PrefixedPath::new(1, vec!(0b1111_1111)).unwrap();
+        assert!(!pref3.is_prefix_of(&full), "prefix3 returned true");
 
-        assert!(pref.is_prefix_of(&full), "prefix returned false");
+        let empty = PrefixedPath::new(0, Vec::new()).unwrap();
+        assert!(empty.is_prefix_of(&full), "empty prefix returned false");
     }
 
     #[test]
