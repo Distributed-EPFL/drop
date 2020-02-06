97ea4c4a5ce0a3a9791cc0fc2adcbff0e51ef019
Ogier Bouvier <ars3nic@ars3nic.ch>
2020-02-06 11:20:56 +0100

    net/connector: implement tcp direct connectivity
    Signed-off-by: Ogier Bouvier <ars3nic@ars3nic.ch>

---
+++ src/net/listener/tcp.rs
@@ -0,0 +1,148 @@
+use std::fmt;
+use std::net::SocketAddr;
+
+use super::super::socket::Socket;
+use super::{Io, Listener, ListenerError};
+use crate::crypto::key::exchange::Exchanger;
+
+use async_trait::async_trait;
+
+use snafu::ResultExt;
+
+use tokio::net::{TcpListener as TokioListener, ToSocketAddrs};
+
+use tracing::{debug, debug_span, info};
+use tracing_futures::Instrument;
+
+/// A plain `TcpListener` that accepts connections on a given IP address and
+/// port
+pub struct TcpListener {
+    listener: TokioListener,
+    exchanger: Exchanger,
+}
+
+impl TcpListener {
+    /// Create a new `TcpListener` that will listen on the candidate address
+    ///
+    /// # Arguments
+    ///
+    /// * `candidate` - The target address to listen on
+    /// * `exchanger` - A key `Exchanger` to be used when handshaking with the
+    /// remote end
+    ///
+    /// # Example
+    /// ```
+    /// use std::net::{Ipv4Addr, SocketAddr};
+    /// use drop::crypto::key::exchange::Exchanger;
+    /// use drop::net::listener::TcpListener;
+    ///
+    /// let addr: SocketAddr = (Ipv4Addr::UNSPECIFIED, 0).into();
+    /// let listener = TcpListener::new(addr, Exchanger::random());
+    /// ```
+    pub async fn new<A: ToSocketAddrs + fmt::Display>(
+        candidate: A,
+        exchanger: Exchanger,
+    ) -> Result<Self, ListenerError> {
+        debug!(
+            "listening with TCP on {} with {}",
+            candidate,
+            exchanger.keypair().public()
+        );
+        TokioListener::bind(candidate)
+            .await
+            .map(|listener| Self {
+                listener,
+                exchanger,
+            })
+            .context(Io)
+    }
+}
+
+#[cfg(unix)]
+mod unix {
+    use std::os::unix::io::{AsRawFd, RawFd};
+
+    use super::TcpListener;
+
+    impl AsRawFd for TcpListener {
+        fn as_raw_fd(&self) -> RawFd {
+            self.listener.as_raw_fd()
+        }
+    }
+}
+
+#[async_trait]
+impl Listener for TcpListener {
+    type Candidate = SocketAddr;
+
+    async fn candidates(&self) -> Result<&[Self::Candidate], ListenerError> {
+        todo!()
+    }
+
+    fn local_addr(&self) -> Option<SocketAddr> {
+        self.listener.local_addr().ok()
+    }
+
+    /// Accept an incoming `Connection` from this `TcpListener` and performs
+    /// key exchange to authenticate the remote peer.
+    async fn establish(&mut self) -> Result<Box<dyn Socket>, ListenerError> {
+        let (stream, remote) = self
+            .listener
+            .accept()
+            .instrument(debug_span!("tcp_accept"))
+            .await
+            .context(Io)?;
+
+        info!("incoming tcp connection from {}", remote);
+
+        Ok(Box::new(stream))
+    }
+
+    fn exchanger(&self) -> &Exchanger {
+        &self.exchanger
+    }
+}
+
+impl fmt::Display for TcpListener {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let addr = self.local_addr().map_or(Err(fmt::Error), Ok)?;
+
+        write!(f, "tcp listener on {}", addr)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test::next_test_ip4;
+
+    #[tokio::test]
+    #[should_panic]
+    async fn tcp_double_bind() {
+        let exchanger = Exchanger::random();
+        let addr = next_test_ip4();
+        let one = TcpListener::new(addr, exchanger.clone())
+            .await
+            .expect("failed to bind");
+
+        let two = TcpListener::new(addr, exchanger)
+            .await
+            .expect("failed to bind");
+
+        assert_eq!(one.local_addr().unwrap(), two.local_addr().unwrap());
+    }
+
+    #[tokio::test]
+    async fn tcp_listener_addr() {
+        let addr = next_test_ip4();
+        let listener = TcpListener::new(addr, Exchanger::random())
+            .await
+            .expect("bind failed");
+
+        assert_eq!(
+            listener.local_addr().unwrap(),
+            addr,
+            "wrong listen address"
+        );
+    }
+}
