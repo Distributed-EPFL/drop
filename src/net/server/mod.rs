0d1c428d6852b3f52b4c6aa0a4e550d9002330d7
Ogier Bouvier <ars3nic@ars3nic.ch>
2020-02-20 17:29:42 +0100

    net: update public API
    Signed-off-by: Ogier Bouvier <ars3nic@ars3nic.ch>

--- src/net/server/mod.rs
+++ src/net/server/mod.rs
@@ -1,16 +1,19 @@
 mod directory;
 pub use self::directory::*;
 
-use std::io::Error as IoError;
+use std::io::Error;
 
 use super::{ReceiveError, SendError};
-use crate as drop;
-use crate::error::Error;
 
-use macros::error;
+use snafu::Snafu;
 
-error! {
-    type: ServerError,
-    causes: (IoError, ReceiveError, SendError),
-    description: "server failure"
+#[derive(Debug, Snafu)]
+/// Errors encountered by servers
+pub enum ServerError {
+    #[snafu(display("i/o error when {}: {}", when, source))]
+    ServerIo { when: String, source: Error },
+    #[snafu(display("error receiving data when {}: {}", when, source))]
+    Receive { when: String, source: ReceiveError },
+    #[snafu(display("error sending data while {}: {}", when, source))]
+    Send { when: String, source: SendError },
 }
