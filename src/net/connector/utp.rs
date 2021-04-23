2b4b5e91dea6e2fbfee062fe87a7acff064c8012
Ogier Bouvier <ogier@bouvier.family>
2021-04-23 14:26:57 +0200

    remove the Wrap connector
    Signed-off-by: Ogier Bouvier <ogier@bouvier.family>

--- src/net/connector/utp.rs
+++ src/net/connector/utp.rs
@@ -1,5 +1,4 @@
-use std::fmt;
-use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, UdpSocket};
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr};
 
 use super::super::socket::utp::BufferedUtpStream;
 use super::*;
@@ -61,43 +60,5 @@ impl Connector for Direct {
 
     fn exchanger(&self) -> &Exchanger {
         &self.exchanger
-    }
-}
-
-pub struct Wrap {
-    exchanger: Exchanger,
-}
-
-#[async_trait]
-impl Connector for Wrap {
-    /// The `Candidate` is a localy bound [`UdpSocket`] and a destination address.
-    /// This is mostly useful when punching holes through NATs since we can
-    /// re-use the socket used to punch the hole to establish a [`Connection`].
-    type Candidate = Info;
-
-    async fn establish(
-        &self,
-        _: &PublicKey,
-        _candidate: &Self::Candidate,
-    ) -> Result<Box<dyn Socket>, ConnectError> {
-        todo!()
-    }
-
-    fn exchanger(&self) -> &Exchanger {
-        &self.exchanger
-    }
-}
-
-/// `Candidate` used for the [`Wrap`] uTP connector
-pub struct Info(UdpSocket, SocketAddr);
-
-impl fmt::Display for Info {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{} -> {}",
-            self.0.local_addr().map_or_else(|_| Err(fmt::Error), Ok)?,
-            self.1
-        )
     }
 }
