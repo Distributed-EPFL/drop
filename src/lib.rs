da3d4fdafb9953624de021727ba71bc43932e9ea
Ogier Bouvier <ogier@bouvier.family>
2021-05-18 13:08:50 +0200

    docs: add feature requirements to documentation
    Signed-off-by: Ogier Bouvier <ogier@bouvier.family>

--- src/lib.rs
+++ src/lib.rs
@@ -29,16 +29,20 @@ pub mod data;
 
 /// Async and synchronous network utilities
 #[cfg(feature = "net")]
+#[cfg_attr(docsrs, doc(cfg(feature = "net")))]
 pub mod net;
 
 /// System management code
 #[cfg(feature = "system")]
+#[cfg_attr(docsrs, doc(cfg(feature = "system")))]
 pub mod system;
 
 #[cfg(any(test, feature = "test"))]
+#[cfg_attr(docsrs, doc(cfg(feature = "test")))]
 /// Test utilities that are used all across the framework
 pub mod test;
 
 /// Re-export `async_trait` to use in implementing custom user types
 #[cfg(feature = "net")]
+#[cfg_attr(docsrs, doc(cfg(feature = "net")))]
 pub use async_trait::async_trait;
